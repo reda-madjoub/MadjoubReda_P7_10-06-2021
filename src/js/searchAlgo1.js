a6c4fe7d55aa1525300e8e0ec1fa1153dca9cc12
Reda M <reda.madjoub@gmail.com>
2021-08-27 19:54:05 +0200

    change main search to use lowerCase

--- src/js/searchAlgo1.js
+++ src/js/searchAlgo1.js
@@ -2,14 +2,15 @@ import { recipes } from './data.js'
 
 export const filterByInput = (userInput = '') => {
     let result = [] 
+    
     for (const item of recipes) {
-        if(item.description.indexOf(userInput) > -1) {
+        if(item.description.toLowerCase().indexOf(userInput.toLowerCase()) > -1) {
             result.push(item.id)
-        }else if(item.name.indexOf(userInput) > -1) {
+        }else if(item.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1) {
             result.push(item.id)
         }else{
             item.ingredients.forEach(elt => {   
-                if(elt.ingredient.indexOf(userInput) > -1) result.push(item.id)
+                if(elt.ingredient.toLowerCase().indexOf(userInput.toLowerCase()) > -1) result.push(item.id)
             })
         }
     }
