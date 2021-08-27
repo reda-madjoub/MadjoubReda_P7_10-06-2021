703d4e3ee5db8d8a42b9f5e7787293ebdf89c891
Reda M <reda.madjoub@gmail.com>
2021-08-27 20:05:33 +0200

    main search use lowercase

--- src/js/searchAlgo2.js
+++ src/js/searchAlgo2.js
@@ -3,18 +3,18 @@ import { recipes } from './data.js'
 export const filterByInput = (userInput = '') => {
     let result = [] 
     for (let i = 0; i < recipes.length; i++) {
-        if(recipes[i].description.indexOf(userInput) > -1) {
+        if(recipes[i].description.toLowerCase().indexOf(userInput.toLowerCase()) > -1) {
             result.push(recipes[i].id)
         }
     }
     for (let i = 0; i < recipes.length; i++) {
-        if(recipes[i].name.indexOf(userInput) > -1) {
+        if(recipes[i].name.toLowerCase().indexOf(userInput.toLowerCase()) > -1) {
             result.push(recipes[i].id)
         }
     }
     for (let i = 0; i < recipes.length; i++) {
         recipes[i].ingredients.forEach(elt => {   
-            if(elt.ingredient.indexOf(userInput) > -1) result.push(recipes[i].id)
+            if(elt.ingredient.toLowerCase().indexOf(userInput.toLowerCase()) > -1) result.push(recipes[i].id)
         })
     }
     return result.filter((item, index) => result.indexOf(item) == index)
